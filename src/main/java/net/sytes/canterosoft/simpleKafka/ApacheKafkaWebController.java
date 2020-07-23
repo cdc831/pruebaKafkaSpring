85f0829f1810f037a323f7004d0cad9fb371eb9e
Cristian Cantero <cdc831@gmail.com>
2020-07-23 14:36:53 -0300

    first commit

---
+++ src/main/java/net/sytes/canterosoft/simpleKafka/ApacheKafkaWebController.java
@@ -0,0 +1,19 @@
+package net.sytes.canterosoft.simpleKafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApacheKafkaWebController {
+
+    @Autowired
+    KafkaSender kafkaSender;
+
+    @GetMapping(value = "/producer")
+    public String producer(@RequestParam("message") String message){
+        kafkaSender.send(message);
+        return "Mensaje enviado";
+    }
+}
